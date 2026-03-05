1e6b5f1e1bd9543a78e020386d957f54494708ae
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-03-05 13:51:05 +0100

    Creation des api utilisateurs

---
+++ ekmer/api/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from views import UserViewSet
+from django.urls import path,include
+
+router = DefaultRouter()
+router.register('users',UserViewSet)
+urlpatterns = [
+    path('',include(router._urls))
+]
