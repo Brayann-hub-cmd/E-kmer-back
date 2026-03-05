663ce0806d4d4e4d1f0e16be79b1a804a00889aa
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-03-05 13:14:09 +0100

    Creation du schema des utilisateurs

--- ekmer/api/models.py
+++ ekmer/api/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-
+import uuid
 class Users(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=128,null=True,blank=True)
+    telephone = models.CharField(max_length=13,null=True,blank=True)
+    email = models.EmailField(max_length=128,null=True,blank=True)
+    password = models.CharField(max_length=16,null=True,blank=True)
+    role = models.CharField(max_length=64,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,editable=False,null=True)
+
+    class Meta:
+        db_table="users"
+        ordering=['-created_at']
