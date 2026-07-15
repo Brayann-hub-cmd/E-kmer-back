e41d7f4d37bc4bba79d50655d9c09356521c53b7
Brayann-hub-cmd <feunguetsamobrayann@gmail.com>
2026-07-15 02:43:08 +0100

    annonces

--- ekmer/ekmer/settings.py
+++ ekmer/ekmer/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
